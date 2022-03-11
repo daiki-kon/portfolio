fd31d269eba10cf8c9dc9097d48005e8b4ed31a2
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2022-03-12 00:04:12 +0900

    style修正

--- src/components/IconName/IconName.tsx
+++ src/components/IconName/IconName.tsx
@@ -9,13 +9,13 @@ const IconName = (props: IconNameProps) => {
   const { name, iconSrc } = props;
   return (
     <>
-      <div className="flex place-items-center flex-col gap-y-1 w-fit ">
+      <div className="flex place-items-center flex-col gap-y-2 w-40 ">
         <Image
           className="mask mask-circle"
           src={iconSrc}
           alt="icon picture"
-          width={150}
-          height={150}
+          width={160}
+          height={160}
         />
         <h2 className="text-2xl">{name}</h2>
       </div>
