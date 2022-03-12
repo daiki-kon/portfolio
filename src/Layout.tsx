08281ab209b55e8c464d458527108f707b24ce96
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2022-03-12 15:54:17 +0900

    navbar固定

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 const Layout: FC = ({ children }) => {
   return (
     <>
-      <nav>
+      <nav className="sticky top-0 z-50">
         <Navbar />
       </nav>
       <main>{children}</main>
