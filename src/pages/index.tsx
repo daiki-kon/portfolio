ee7391ee1880af25c900bfa951cf00513e56a16e
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2022-03-20 21:33:29 +0900

    index作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,48 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import AboutMeSection from '../components/AboutMeSection';
+import CertificationSection from '../components/CertificationSection';
+import Footer from '../components/Footer';
+import SkillSection from '../components/SkillSection';
+import {
+  iconImageSrc,
+  introduction,
+  myName,
+  snsAccounts,
+} from '../data/AboutMe';
+import { badgeImages, certifications } from '../data/Certifications';
+import { skills } from '../data/Skills';
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Hiroki Kondo portfolio</title>
+      </Head>
+      <main className="container mx-auto lg:max-w-5xl mb-11">
+        <section className="mt-11">
+          <AboutMeSection
+            introduction={introduction}
+            name={myName}
+            iconImageSrc={iconImageSrc}
+            snsAccounts={snsAccounts}
+          />
+        </section>
+
+        <section className="mt-11">
+          <SkillSection skills={skills} />
+        </section>
+
+        <section className="mt-11">
+          <CertificationSection
+            badgeImages={badgeImages}
+            certifications={certifications}
+          />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
