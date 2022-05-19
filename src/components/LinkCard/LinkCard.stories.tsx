0a6f98c9b239e6e3a69af3ed41415d418a37c522
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2022-05-19 20:20:06 +0900

    レスポンシブLinkCard作成

--- src/components/LinkCard/LinkCard.stories.tsx
+++ src/components/LinkCard/LinkCard.stories.tsx
@@ -19,6 +19,7 @@ export const Primary = Template.bind({});
 Primary.args = {
   imageUrl:
     'https://qiita-user-contents.imgix.net/https%3A%2F%2Fcdn.qiita.com%2Fassets%2Fpublic%2Farticle-ogp-background-9f5428127621718a910c8b63951390ad.png?ixlib=rb-4.0.0&w=1200&mark64=aHR0cHM6Ly9xaWl0YS11c2VyLWNvbnRlbnRzLmltZ2l4Lm5ldC9-dGV4dD9peGxpYj1yYi00LjAuMCZ3PTkxNiZ0eHQ9RHluYW1vREIlRTMlODElQUUlRTQlQjglQTYlRTUlODglOTclRTMlODIlQjklRTMlODIlQUQlRTMlODMlQTMlRTMlODMlQjMlRTMlODIlOTJib3RvMyVFMyU4MSVBNyVFOCVBOSVBNiVFMyU4MSU5NyVFMyU4MSVBNiVFMyU4MSVCRiVFMyU4MSU5RiZ0eHQtY29sb3I9JTIzMjEyMTIxJnR4dC1mb250PUhpcmFnaW5vJTIwU2FucyUyMFc2JnR4dC1zaXplPTU2JnR4dC1jbGlwPWVsbGlwc2lzJnR4dC1hbGlnbj1sZWZ0JTJDdG9wJnM9ZDg1NWVkNjcwM2M0ZDNlN2ZlNmRiMzczOTEyOTVkODU&mark-x=142&mark-y=112&blend64=aHR0cHM6Ly9xaWl0YS11c2VyLWNvbnRlbnRzLmltZ2l4Lm5ldC9-dGV4dD9peGxpYj1yYi00LjAuMCZ3PTYxNiZ0eHQ9JTQwZGFpa2kta29uJnR4dC1jb2xvcj0lMjMyMTIxMjEmdHh0LWZvbnQ9SGlyYWdpbm8lMjBTYW5zJTIwVzYmdHh0LXNpemU9MzYmdHh0LWFsaWduPWxlZnQlMkN0b3Amcz1jZTBlMWU1OWFjNjZjOTg1NTZkYjM5Yzg1ZWQyYWYwMw&blend-x=142&blend-y=491&blend-mode=normal&s=00d82702028eb77de88af3666f1885a8',
+  smallImage: '/qiita-favicon.png',
   title: 'DynamoDBの並列スキャンをboto3で試してみた - Qiita',
   description:
     '\nはじめに\nDynamoDBでデータを取得する際scanを使用するとテーブルの全てのレコードに対して逐次処理をしていきます。\nそのため基本的にはscanではなく、queryを使うというのがベストプラクティスです。\nしかし、どうしてもス...',
--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 type LinkCardProps = {
   imageUrl: string;
+  smallImage?: string;
   title: string;
   description: string;
   url: string;
@@ -9,21 +10,43 @@ type LinkCardProps = {
 };
 
 const LinkCard = (props: LinkCardProps) => {
-  const { imageUrl, title, description, updateAt, url } = props;
+  const { imageUrl, smallImage, title, description, updateAt, url } = props;
+
+  const urlHost = new URL(url).host;
+
   return (
     <>
-      <a className="flex cursor-pointer hover:bg-base-300 dark:hover:bg-neutral-focus ">
-        <Image
-          src={imageUrl}
-          width={200}
-          height={120}
-          alt="qiita article"
-          objectFit="contain"
-        />
-        <div>
-          <h1>{title}</h1>
-          <p className=''>{description}</p>
-          <p>{url}</p>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="card card-side cursor-pointer hover:bg-base-300 dark:hover:bg-neutral-focus "
+      >
+        <div className="hidden md:flex justify-center mr-1">
+          <Image
+            src={imageUrl}
+            width={300}
+            height={150}
+            alt="qiita article"
+            objectFit="cover"
+          />
+        </div>
+
+        {smallImage !== undefined && (
+          <div className="md:hidden flex justify-center mr-1">
+            <Image
+              src={smallImage}
+              width={50}
+              height={50}
+              alt="qiita article"
+              objectFit="contain"
+            />
+          </div>
+        )}
+        <div className="card-body max-w-xl p-2.5 ">
+          <h2 className="card-title text-sm">{title}</h2>
+          <p className="truncate hidden md:inline">{description}</p>
+          <p>{urlHost}</p>
           <p>{updateAt}</p>
         </div>
       </a>
